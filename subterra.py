7f65c70598d9f7cffbcb6f7f93f1896f7f344c88
joelsherwin <sherwinjoel@gmail.com>
2017-02-26 20:30:17 +0530

    Update subterra.py

--- subterra.py
+++ subterra.py
@@ -5,8 +5,9 @@ import hashlib
 import time
 import urllib2
 import sys
-arr1 = os.listdir("I:\Movies")
-prePath = "I:\Movies\\"
+
+prePath = "I:\Movies\\"  #change this directory to yours
+arr1 = os.listdir(prePath)
 replace = [".avi",".mp4",".mkv",".mpg",".mpeg",".mov"]
 
 
@@ -15,7 +16,6 @@ def calc_hash(filepath):
     with open(filepath,'rb') as file:
         size = os.path.getsize(filepath)
         data = file.read(readsize)
-
         file.seek(-readsize,os.SEEK_END)
         data += file.read(readsize)
         return hashlib.md5(data).hexdigest()
@@ -24,7 +24,6 @@ def get_subtitles(path):
     i=0
     print 'Calculating hash of video file...'
     hash = calc_hash(path)
-    replace = [".avi",".mp4",".mkv",".mpg",".mpeg",".mov"]
     for c in replace:
         path = path.replace(c,"")
     if os.path.exists(path+".srt"):
@@ -38,11 +37,10 @@ def get_subtitles(path):
             i = i+1
         print ']'
         print 'Subtitle Downloaded'
-        headers = { 'User-Agent' : 'SubDB/1.0 (subtitle-downloader/1.0; Subterra_v0.1a)' }
+        headers = { 'User-Agent' : 'SubDB/1.0 (subterra/0.1a; Subterra_v0.1a)' }
         url = "http://api.thesubdb.com/?action=download&hash="+hash+"&language=en"
         req = urllib2.Request(url, '', headers)
         response = urllib2.urlopen(req).read()
-
         with open(path+".srt","wb") as file1:
             file1.write(response)
             
